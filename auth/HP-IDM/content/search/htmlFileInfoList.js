45356c1471fb3e06d6fd6d6259b32af4360b4f39
Jorge L. Williams <jorge.williams@rackspace.com>
2011-12-23 12:28:30 -0600

    Add HP-IDM extension.

---
+++ auth/HP-IDM/content/search/htmlFileInfoList.js
@@ -0,0 +1,3 @@
+fil = new Array();
+fil["0"]= "AboutExtension.html@@@Chapter 1. About This Extension - OpenStack Identity HP-IDM Extension@@@null";
+fil["1"]= "ch02.html@@@Chapter 2. Summary of Changes - OpenStack Identity HP-IDM Extension@@@null";
---
+++ auth/HP-IDM/content/search/htmlFileList.js
@@ -0,0 +1,4 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "AboutExtension.html";
+fl["1"]= "ch02.html";
